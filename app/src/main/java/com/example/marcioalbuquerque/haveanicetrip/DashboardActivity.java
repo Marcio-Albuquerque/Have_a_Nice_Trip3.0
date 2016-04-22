c3e71a7490a80c1f6d13535aa3d12b1dd00bdec9
Marcio-Albuquerque <marcio.albu@alu.ufc.br>
2016-04-22 18:11:46 -0300

    Terceiro Layout Viagem criado e completo;
    Quarto Layout  criada e ainda incompleto;
    
     Dashboard ativado a função butão

--- app/src/main/java/com/example/marcioalbuquerque/haveanicetrip/DashboardActivity.java
+++ app/src/main/java/com/example/marcioalbuquerque/haveanicetrip/DashboardActivity.java
@@ -1,11 +1,14 @@
 package com.example.marcioalbuquerque.haveanicetrip;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -13,7 +16,25 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
     }
 
+    public void selecionarOpcao(View view) {
+        /*
+        com base na view que foi clicada
+        iremos tomar a ação correta
+        */
+
+        switch (view.getId()) {
+            case R.id.nova_viagem:
+                startActivity(new Intent(this, ViagemActivity.class));
+                break;
+            case R.id.novo_gasto:
+                startActivity(new Intent(this, GastoActivity.class));
+                break;
+        }
+
+        TextView textView = (TextView) view;
+        String opcao = "Opção: " + textView.getText().toString();
+        Toast.makeText(this, opcao, Toast.LENGTH_LONG).show();
+    }
 }
