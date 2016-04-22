47528a809d8b36d02fd9b343a08bcedfef89bb59
Marcio-Albuquerque <marcio.albu@alu.ufc.br>
2016-04-22 13:59:21 -0300

    Primeiro Layout Atualizada Cor Buttom Segunda Layout Dashboard completamente criada

--- app/src/main/java/com/example/marcioalbuquerque/haveanicetrip/NiceTripActivity.java
+++ app/src/main/java/com/example/marcioalbuquerque/haveanicetrip/NiceTripActivity.java
@@ -1,5 +1,6 @@
 package com.example.marcioalbuquerque.haveanicetrip;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +9,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NiceTripActivity extends AppCompatActivity {
 
+    private EditText usuario;
+    private EditText senha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nicetrip);
+        usuario = (EditText) findViewById(R.id.usuario);
+        senha = (EditText) findViewById(R.id.senha);
+    }
 
+    public void entrarOnClick(View v) {
+        String usuarioInformado = usuario.getText().toString();
+        String senhaInformada = senha.getText().toString();
+        if("leitor".equals(usuarioInformado) && "123".equals(senhaInformada)) {
+            startActivity(new Intent(this,DashboardActivity.class));
+        } else {
+            String mensagemErro = getString(R.string.erro_autenticacao);
+            Toast toast = Toast.makeText(this, mensagemErro, Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 }
