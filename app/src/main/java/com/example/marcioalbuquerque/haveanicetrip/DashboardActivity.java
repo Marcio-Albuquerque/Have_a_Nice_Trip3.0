63cf2fdeeaabe26bce584f9bd5bc5e7dbeab457e
Marcio-Albuquerque <marcio.albu@alu.ufc.br>
2016-04-22 15:12:14 -0300

    Primeiro Layout Atualizada Cor Buttom Segunda Layout Dashboard completamente criada
    Consertando Erros da fotso png e da cor primeirias

--- app/src/main/java/com/example/marcioalbuquerque/haveanicetrip/DashboardActivity.java
+++ app/src/main/java/com/example/marcioalbuquerque/haveanicetrip/DashboardActivity.java
@@ -13,6 +13,7 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
     }
 
 }
